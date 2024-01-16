3329d3d413aa26af82a5ab7b616f8e8f7a376f8d
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-01-16 23:31:33 +0300

    feat: add css scss style loader / extract plugin

--- build/main.e29dae63098bccddd8fc.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("Pavel Topsky!"),console.log(87)})();
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 type Mode = "production" | "development";
@@ -12,6 +13,7 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
   const isDev = env.mode === "development";
+  const isProd = env.mode === "production";
 
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
@@ -24,9 +26,18 @@ export default (env: EnvVariables) => {
     plugins: [
       new HtmlWebpackPlugin({ template: path.resolve(__dirname, "public", "index.html") }),
       isDev && new webpack.ProgressPlugin(),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: "css/[name].[contenthash:8].css",
+          chunkFilename: "css/[name].[contenthash:8].css",
+        }),
     ].filter(Boolean),
     module: {
       rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [isDev ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
         {
           test: /\.tsx?$/,
           use: "ts-loader",
