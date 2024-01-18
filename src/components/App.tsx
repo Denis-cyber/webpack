bf6dbaf42c590496116a00ab92f659ee1e5740b1
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-01-18 23:56:46 +0300

    feat: add router/lazy chunks/bundle analyzer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 import classes from "./App.module.scss";
 
 export const App = () => {
@@ -6,10 +7,14 @@ export const App = () => {
 
   return (
     <div>
+      <Link to='/about'>about</Link>
+      <br />
+      <Link to='/shop'>shop</Link>
       <h1 className={classes.value}>{count}</h1>
       <button className={classes.button} onClick={() => setCount((prev) => prev + 1)}>
         Increment
       </button>
+      <Outlet />
     </div>
   );
 };
